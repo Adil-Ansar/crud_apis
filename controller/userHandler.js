70c4b3b377ad64f8ba7e30b4b30a14b5cc8e00cc
Adil-Ansar <ansari.adil2015@gmail.com>
2023-12-20 20:43:21 +0530

    Added getUSers APIs

--- controller/userHandler.js
+++ controller/userHandler.js
@@ -43,7 +43,75 @@ const createUser = async (req, res) => {
     }
 }
 
+// Function to get all user details
+const getAllUser = async (req, res) => {
+    try {
+        // Check if the 'data' array is empty
+        if (!data.length) {
+            // If 'data' is empty, return a success response indicating no users found
+            return res.status(200).json({
+                meta: { msg: "Users not found", status: false }
+            });
+        } else {
+            // If 'data' has user records, return all user data with a success status
+            return res.status(200).json({
+                meta: { msg: "All users data", status: true },
+                data
+            });
+        }
+    } catch (error) {
+        // Handle any unexpected errors and return a 500 status with an error message
+        return res.status(500).json({
+            meta: { msg: "Something went wrong.", status: false },
+            data: error.message
+        });
+    }
+}
+
+
+
+// Function to get user details by ID
+const getUserDetails = async (req, res) => {
+    try {
+        // Extract userId from request parameters
+        const { userId } = req.params;
+
+        // Check if userId is not a number
+        if (isNaN(userId)) {
+            return res.status(400).json({
+                meta: { msg: "Invalid userId", status: false },
+            });
+        }
+
+        // Find user data by ID in the 'data' array
+        const userData = data.find(obj => obj.id === Number(userId));
+
+        // If user data is found, return it with a success status
+        if (userData) {
+            return res.status(200).json({
+                meta: { msg: "User found", status: true },
+                data: userData
+            });
+        } else {
+            // If user data is not found, return a 404 status with a relevant message
+            return res.status(404).json({
+                meta: { msg: "User not found", status: false }
+            });
+        }
+    } catch (error) {
+        // Handle any unexpected errors and return a 500 status with an error message
+        return res.status(500).json({
+            meta: { msg: "Something went wrong.", status: false },
+            data: error.message
+        });
+    }
+}
+
+
+
 // Export the createUser function to use it elsewhere in the application
 module.exports = {
-    createUser
+    createUser,
+    getAllUser,
+    getUserDetails
 };
--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,9 +1,16 @@
 const userRoutes = require("express").Router();
 
-const { createUser } = require("./../controller/userHandler");
+const {
+    createUser,
+    getAllUser,
+    getUserDetails
+} = require("./../controller/userHandler");
 const basePath = "/api";
 
 userRoutes.post("/user", createUser);
+userRoutes.get("/user", getAllUser);
+userRoutes.get("/user/:userId", getUserDetails);
+
 
 module.exports = {
     userRoutes,
